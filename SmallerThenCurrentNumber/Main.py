e47ef65b7302c3ede4e3a876c54a51522a2e9e83
sAayush <aayushsoni4116@gmail.com>
2025-12-09 12:54:24 +0530

    Refactor smallerNumbersThanCurrent method to improve performance and remove unnecessary code

--- SmallerThenCurrentNumber/Main.py
+++ SmallerThenCurrentNumber/Main.py
@@ -3,14 +3,36 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        # ans = []
+        # size = len(nums)
+        # for num in nums:
+        #     count = 0
+        #     for i in range(size):
+        #         if num > nums[i]:
+        #             count += 1
+        #     ans.append(count)
+        # return ans
+    
+        ht = [0] * 101
         ans = []
-        size = len(nums)
-        for num in nums:
-            count = 0
-            for i in range(size):
-                if num > nums[i]:
-                    count += 1
-            ans.append(count)
+
+        for n in nums:
+            ht[n]+=1
+        
+        # print('ht new: ', ht)
+        
+        accumulator = 0
+        for i in range(101):
+            if ht[i] > 0:
+                temp = ht[i]
+                ht[i] = accumulator
+                accumulator+=temp
+                # print(ht, accumulator, temp)
+        
+        for n in nums:
+            ans.append(ht[n])
+        
+
         return ans
     
 if __name__ == "__main__":
